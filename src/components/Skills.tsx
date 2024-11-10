7bd40fdd4a104ac00bc85652de57808c52dadb4d
wajeeha-khalid <165416601+wajiha-khalid@users.noreply.github.com>
2024-11-10 21:30:20 +0500

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,7 +6,7 @@ const Skills = () => {
         <div className='grid md:grid-cols-2 gap-20 items-center'>
             <div  data-aos="zoom-in-up">
                 <h2 className='text-4xl md:text-5xl'>Technologies I work with</h2>
-            <p className='text-gray-500 pt-2'>I have a foundation in HTML, CSS, TypeScript, Next.js, and Tailwind CSS, which I am actively working to strengthen. I have applied these skills to create interactive, responsive web interfaces. While my proficiency is still growing, I am dedicated to improving and delivering clean, efficient code. I am excited to expand my knowledge in modern web development."
+            <p className='text-gray-500 pt-2'>I have a foundation in HTML, CSS, TypeScript, Next.js, and Tailwind CSS, which I am actively working to strengthen. I have applied these skills to create interactive, responsive web interfaces. While my proficiency is still growing, I am dedicated to improving and delivering clean, efficient code. I am excited to expand my knowledge in modern web development.
               </p>
             </div>
             <div>
