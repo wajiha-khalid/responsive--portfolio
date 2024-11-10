507e7d5916834ea2fb80a4eb9cc1bd760ceb908d
wajeeha-khalid <165416601+wajiha-khalid@users.noreply.github.com>
2024-11-10 21:16:29 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div id='about' className='container pt-32' >
           <h2 className='text-4xl md:text-5xl' data-aos="zoom-in-up">About Me</h2>
           <p className='text-gray-500 pt-4' data-aos="zoom-in-up">
-            Iam an aspiring developer currently enrolled in the GIAIC Web 3.0, Metaverse, and Artificial Intelligence course. With a passion for technology, I’m motivated to explore emerging fields and build my skills step by step. I have a foundational understanding of frontend development and am committed to improving my abilities and gaining confidence as I grow. Through continued learning,
+            Iam an aspiring developer currently enrolled in the GIAIC Web 3.0 Metaverse and Artificial Intelligence course. With a passion for technology, I am motivated to explore emerging fields and build my skills step by step. I have a foundational understanding of frontend development and am committed to improving my abilities and gaining confidence as I grow. Through continued learning,
              </p>      
     </div>
   )
